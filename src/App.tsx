0c2c78b557fbe0aa86130ee16dbbd866023d01b5
Wagner Lima <wagner.sousalima@gmail.com>
2023-07-06 17:30:25 -0300

    conteiner base

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-
-
 function App() {
-
   return (
-    <>
-    
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-    </>
-  )
+    <main className="flex justify-center items-center
+    bg-gradient-to-br from-yellow-200 to-transparent border-6 border-black
+    rounded-xl shadow-md h-[100vh] w-full">
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center 
+      items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white
+      bg-opacity-20 backdrop:blur-lg rounded drop-shadow-lg text-zinc-700">
+        Weather App
+     </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
